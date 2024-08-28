6819976e65708234d701d83ef368aa633a490ea3
Devin Lepur <devin.lepur7@gmail.com>
2024-08-28 15:58:44 -0700

    Attempting src fix

--- tests/test_edge_detection.py
+++ tests/test_edge_detection.py
@@ -2,13 +2,9 @@
 Test file for edge detection
 '''
 
-import cv2
 import os
-import sys
 
-# Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from src.edge_detection import edge_detection
+from ..src.edge_detection import edge_detection
 
 def test_edge_detection():
     input_image = "tests/input_test.jpg"
